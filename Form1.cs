196322212f9327f4dad61d4cf68cd5f41493b7f2
mccreery-john-student <70606724+mccreery-john-student@users.noreply.github.com>
2023-11-27 01:57:42 -0500

    Commit for the night

--- Form1.cs
+++ Form1.cs
@@ -1,6 +1,23 @@
 namespace FM24_Coach_Calculator
 {
-
+    static class Coach
+    {
+        public static int attacking = 1;
+        public static int defending = 1;
+        public static int mental = 1;
+        public static int tactical = 1;
+        public static int technical = 1;
+        public static int fitness = 1;
+        public static int setPieces = 1;
+        public static int tactKnow = 1;
+        public static int gkDistribution = 1;
+        public static int gkHandling = 1;
+        public static int gkShotStopping = 1;
+        public static int determination = 1;
+        public static int levelOfDisc = 1;
+        public static int motivating = 1;
+        
+    }
 
     public partial class Form1 : Form
     {
@@ -22,32 +39,7 @@ namespace FM24_Coach_Calculator
 
 
 
-        public class Coach
-        {
-            void coachAttributes()
-            {
-                int attacking = 1;
-                int defending = 1;
-                int mental = 1;
-                int tactical = 1;
-                int technical = 1;
-                int fitness = 1;
-                int setPieces = 1;
-                int tactKnow = 1;
-                int gkDistribution = 1;
-                int gkHandling = 1;
-                int gkShotStopping = 1;
-                int determination = 1;
-                int levelOfDisc = 1;
-                int motivating = 1;
-            }
-
-            public Coach()
-            {
-                this.coachAttributes();
-            }
-
-        }
+        
 
 
 
@@ -58,7 +50,28 @@ namespace FM24_Coach_Calculator
 
         private void attText_TextChanged(object sender, EventArgs e)
         {
-            
+            string newValue = attText.Text;
+            int attack = Coach.attacking;
+            try
+            {
+                attack = int.Parse(newValue);
+            }
+            catch (FormatException ex )
+            {
+                attText.Text = Coach.attacking.ToString();
+                return;
+            }
+
+            if (attack > 20)
+            {
+                attack = 20;
+            }else if (attack < 1)
+            {
+                attack = 1;
+            }
+
+            attText.Text = attack.ToString();
+            Coach.attacking = attack;
         }
     }
 }
